ab529d1ee7bc5b20a691a5d49c350a72eb089628
Kyung-ji-Kang <129140459+Kyung-ji-Kang@users.noreply.github.com>
2025-03-15 03:33:30 +0900

    Title: 요일 맞추기, Time: 117ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250315/요일 맞추기/guess-day-of-week.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+public class Main {
+
+    public static String what_day(int result) {
+        if (result == 0)
+            return "Mon";
+        else if (result == 1)
+            return "Tues";
+        else if (result == 2)
+            return "Wed";
+        else if (result == 3)
+            return "Thu";
+        else if (result == 4)
+            return "Fri";
+        else if (result == 5)
+            return "Sat";
+        else
+            return "Sun"; 
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+        // Please write your code here.
+        // 1. 월마다 몇일이 있는지 확인 해야함
+        // 2. +7 마다 월요일이 돌아옴 
+        int[] num_of_days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        
+
+        int days1 = d1;
+        int days2 = d2;
+        for(int i=1; i<m1; i++){
+            days1 += num_of_days[i];
+        } 
+        for(int i=1; i<m2; i++){
+            days2 += num_of_days[i];
+        } 
+        int result = days2 - days1 ;
+        result = ((result % 7) + 7) % 7;
+
+        System.out.println(what_day(result));
+            
+    }
+}
