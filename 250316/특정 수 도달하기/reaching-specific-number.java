10e939963227d571dd56abed5dae3103b8211f26
Kyung-ji-Kang <129140459+Kyung-ji-Kang@users.noreply.github.com>
2025-03-16 01:16:11 +0900

    Title: 특정 수 도달하기, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250316/특정 수 도달하기/reaching-specific-number.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    static final int N = 10;
+
+    public static void main(String[] args) {
+        // Please write your code here.
+        
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr =new int[N];
+
+        int sum=0;
+        int count=0;
+
+        for(int i=0; i<N; i++){
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+            
+            if(arr[i]>=250){
+                sum -= arr[i];
+                count = i;
+                break;
+            }
+            count++;
+        }
+
+        float aver = (float)sum/count;
+       System.out.println(sum+" "+aver); // "3.1
+    }
+}
